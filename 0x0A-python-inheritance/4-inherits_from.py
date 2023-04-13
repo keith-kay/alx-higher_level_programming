46bc73b7496155ffc9c3409abd3fa3ec4ef2ed1a
keith-kay <keithrhova@gmail.com>
2023-04-13 12:29:18 -0700

    fourth commit update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ Module with function inherits_from
 def inherits_from(obj, a_class):
     """function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False."""
 
-    return False if type(obj) is a_class else isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
