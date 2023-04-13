e6bdd4063b03eec56ea633eb91d6cbe64f41c043
keith-kay <keithrhova@gmail.com>
2023-04-13 12:38:52 -0700

    task 6 commit

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+
+"""BaseGeometry empty class Module"""
+
+
+class BaseGeometry():
+    """ empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+
+"""BaseGeometry class Module based on task 5"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+
+    def area(self):
+        """methid for calculated area"""
+
+
+        raise Exception("area() is not implemented")
