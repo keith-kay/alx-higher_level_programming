0e72cbbdb6c74a26036607ac0d15f6460e9b10e2
keith-kay <keithrhova@gmail.com>
2023-04-13 12:16:27 -0700

    second commit update

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,10 @@
 #!/bin/usr/python3
 # 2-is_same_class.py
 
+"""Module containing is_same_class method"""
+
 def is_same_class(obj, a_class):
     
     """Return True if obj is an instance of a_class, otherwise False"""
 
-    return type(obj) is a_class
+    return type(obj) == a_class
