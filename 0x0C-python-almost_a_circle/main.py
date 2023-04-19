4bcc61fae4bb8d147435db9ba0eb725702f10f65
keith-kay <keithrhova@gmail.com>
2023-04-18 23:33:20 -0700

    commit update

---
+++ 0x0C-python-almost_a_circle/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 4-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(4, 6)
+    r1.display()
+
+    print("---")
+
+    r1 = Rectangle(2, 2)
+    r1.display()
+
+guillaume@ubuntu:~/$ ./4-main.py
+####
+####
+####
+####
+####
+####
+---
+##
+##
