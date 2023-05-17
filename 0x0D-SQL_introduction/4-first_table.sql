d4ed6b813b405ac93bd5d77aeb9d0ed1f9a8260c
keith-kay <keithrhova@gmail.com>
2023-05-16 23:05:18 -0700

    task 5

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Create a table called first_table
+CREATE table IF NOT EXISTS first_table(`id` INT, `name` VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- print full description of first table
+SHOW CREATE table first_table;
