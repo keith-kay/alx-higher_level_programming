f10e676c0ad3cf49319fe3e2769fe69d47fbfe42
keith-kay <keithrhova@gmail.com>
2023-06-14 12:02:31 -0700

    update task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script must import dict from the file 101-data.js
-const dict = require('./101-data.js').dict;
+const dict = required('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
