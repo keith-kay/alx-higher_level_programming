e7167a5387b58291bab4a4f1fcbc804c7eee6f92
keith-kay <keithrhova@gmail.com>
2023-06-14 11:37:52 -0700

    advanced task commit

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// a script that imports an array and computes a new array. 100-data.js
+exports.list = [1, 2, 3, 4, 5];
---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script must import dict from the file 101-data.js
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
