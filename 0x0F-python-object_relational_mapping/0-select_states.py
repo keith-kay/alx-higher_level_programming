f822edcb85b5ee9c78b76dd2f61a6238b703c84a
keith-kay <keithrhova@gmail.com>
2023-06-19 00:19:19 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,24 +3,22 @@
 script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 
 """
-import sys
 import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
 
-    cursor = db.cursor()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    data = cursor.fetchall()
-
-    for row in data:
-        print(row)
-
-    cursor.close()
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    
+    cur.close()
     db.close()
+
+
