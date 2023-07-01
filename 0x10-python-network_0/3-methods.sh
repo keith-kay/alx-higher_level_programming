845545cecbc06bf2b8c82ada9d29c530a766eb75
keith-kay <keithrhova@gmail.com>
2023-07-01 15:02:17 -0700

    task update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bi/bash
-#  script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep Allow | cut -d ' ' -f2-
+#  script that takes in a URL and displays all HTTP methods the server will accept 
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
