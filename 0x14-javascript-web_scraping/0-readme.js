3df3e3a2f9fa00f52952ab65f789b740d42e3cf8
keith-kay <keithrhova@gmail.com>
2023-07-25 14:42:33 -0700

    final commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 // script that reads and prints the content of a file.
 
 const argv = process.argv;
-const fs = require('fs'); // Use const instead of let for fs
+const fs = require('fs');
 fs.readFile(argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
