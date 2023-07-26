8e43dc6ea788362f80f1c52c6fd60b8c2c537783
keith-kay <keithrhova@gmail.com>
2023-07-26 13:08:14 -0700

    update commit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,20 +2,8 @@
 // Writes a string to a file
 
 const fs = require('fs');
-const argv = process.argv;
-
-if (argv.length !== 4) {
-  console.error('Usage: node script.js <file_path> <string_to_write>');
-  process.exit(1);
-}
-
-const filePath = argv[2];
-const string = argv[3];
-
-fs.writeFile(filePath, string, 'utf8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], err => {
   if (err) {
-    console.error('Error writing to file:', err);
-  } else {
-    console.log('String has been written to the file successfully.');
+    console.log(err);
   }
 });
